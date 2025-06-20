fcf615352cccf3932af18b829ba3857d701df5cb
liuzibin <liuzibin011@gmail.com>
2025-06-20 22:51:55 +0100

    安装pinia

--- Frontend/Minutes 0.2/src/main.ts
+++ Frontend/Minutes 0.2/src/main.ts
@@ -7,12 +7,17 @@ import router from './router'
 // 如果你使用 Element Plus：
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+//引入pinia
+import { createPinia } from 'pinia'
 
 //创建一个app
 const app =createApp(App)
+const pinia =createPinia();
 // app安装插件：使用路由和 Element Plus（或其他组件库）
 app.use(router)
 app.use(ElementPlus)
+
+app.use(pinia); // ✅ 直接传入 createPinia() 的结果
+
 //挂载整个app到id=app中
 app.mount('#app')
