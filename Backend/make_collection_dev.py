469c74a04bcf4af680f5145cc01286977caa233e
chrisheng456 <gg24695@bristol.ac.uk>
2025-06-20 15:21:19 +0100

    rename make_collection.py

--- Backend/make_collection_dev.py
+++ Backend/make_collection_dev.py
@@ -8,12 +8,12 @@ dev = ir_datasets.load("msmarco-passage/dev/small")
 relevant_pids = sorted({int(q.doc_id) for q in dev.qrels_iter() if q.relevance > 0})
 print(f"→ Got {len(relevant_pids)} PIDs from dev-small.")
 
-# 2. 遍历 dev-small docs_iter，只写相关的
-with open("collection_dev.tsv", "w", encoding="utf-8") as fout:
+# 2. 遍历 dev-small docs_iter，只写相关的到 collection.tsv
+with open("collection.tsv", "w", encoding="utf-8") as fout:
     for doc in dev.docs_iter():
         pid = int(doc.doc_id)
         if pid in relevant_pids:
             text = doc.text.replace("\n", " ")
             fout.write(f"{pid}\t{text}\n")
 
-print(f"✅ Wrote collection_dev.tsv with {len(relevant_pids)} lines.")
+print(f"✅ Wrote collection.tsv with {len(relevant_pids)} lines.")
